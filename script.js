c3f1acb5123b053ed910eb6ea57e99da2aaebb54
name <97321565+Jbsousa86@users.noreply.github.com>
2025-03-04 17:20:10 -0300

    add botao

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById("whatsappButton").addEventListener("click", function(){
+document.getElementById("button").addEventListener("click", function(){
     window.open("https://wa.me/5563992225774?text=Bem%20vindo!")
 });
